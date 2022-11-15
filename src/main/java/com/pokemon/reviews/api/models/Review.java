144b2b5668f0f4feec879b1fa6aec13a508bfd79
sphavix <zwelakhespha@gmail.com>
2022-11-15 16:22:06 +0200

    Initial Commit

---
+++ src/main/java/com/pokemon/reviews/api/models/Review.java
@@ -0,0 +1,23 @@
+package com.pokemon.reviews.api.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String title;
+    private String content;
+    private int stars;
+}
